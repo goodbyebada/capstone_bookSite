b7d79e4e38763d6ad6e5639e1a41ad18febea4c1
goodbyebada <goodbyebada@naver.com>
2024-05-31 16:57:40 +0900

    frontEnd 비슷한책 추천 api 적용

--- frontEnd/src/data/const.ts
+++ frontEnd/src/data/const.ts
@@ -1,4 +1,4 @@
 export const HEADER_HEIGHT = "50px";
 export const initYear = "2000";
 export const useDummy = false;
-export const Api2Url = "";
+export const Api2Url = `http://localhost:8080/books/similarBooks`;
